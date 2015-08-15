96e8d7ea7d47be1c6b390e900b98362e82f68f3c
Jai Singh <jai@pubnub.com>
2015-08-15 02:30:15 -0700

    docs

--- todo.bash
+++ todo.bash
@@ -2,6 +2,9 @@
 # Edit files with the .md extension
 # Todo items begin with a +
 # Needs you GOPATH setup and the library below
+# Usage:
+# todo - lists all todo items
+# todo # - edit the file with the todo line
 
 function todo (){
 colorize="$GOPATH/src/github.com/jaisingh/colorize/colorize"
