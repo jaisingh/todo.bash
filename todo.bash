2d0bfacf0da9ace051a4e74d2816ed21179c1cf8
Jai Singh <jai@pubnub.com>
2015-08-15 02:27:01 -0700

    first rev

---
+++ todo.bash
@@ -0,0 +1,17 @@
+# A simple todo system
+# Edit files with the .md extension
+# Todo items begin with a +
+# Needs you GOPATH setup and the library below
+
+function todo (){
+colorize="$GOPATH/src/github.com/jaisingh/colorize/colorize"
+list=$(find ~/Documents -iname "*.md" -print | xargs egrep "^(\+|\s+\+)")
+
+if [ -z $1 ]
+then
+	echo "$list" | nl | column -t -s ";" | $colorize -color='default:236'
+	return
+fi
+
+echo "$list" | sed -n $1p | cut -d : -f 1 | xargs -o vim
+}
